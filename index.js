8f26c0cddbd16737811a153a02466a5725c95095
lisa <lisaauta018@gmail.com>
2024-10-11 19:10:39 +0300

    first-trial

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (numberOne, numberTwo)=> numberOne / numberTwo;
+divide(2000, 100);
+
+const square = (NumberOne)=> NumberOne ** 2;
+square(9);
+
+const add = (Numberone, Numbertwo)=> Numberone + Numbertwo;
+add (8, 9)
